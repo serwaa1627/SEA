9ec047a0a8c47b2fa0e11cd78541adc1b4dfc528
serwaa-a <103071506+serwaa-a@users.noreply.github.com>
2025-06-27 10:48:55 +0100

    ammended loaduser function

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[
